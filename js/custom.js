4c6d3633b4b718b27a4686406fa8fc4be4683f72
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-12-07 00:34:59 -0600

    updated the contact section

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 
 
 
+//Adds the Hyphen's in the phone number on the contact page 
+
+	function addHyphen (element) {
+    	let ele = document.getElementById(element.id);
+        ele = ele.value.split('-').join('');    // Remove dash (-) if mistakenly entered.
+
+        let finalVal = ele.replace(/(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)/, '$1$2$3-$4$5$6-$7$8$9$10')
+        document.getElementById(element.id).value = finalVal;
+    }
